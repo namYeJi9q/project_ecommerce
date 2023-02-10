9e15a990f9ec5ac8c7b3b65034a00fbad237d93a
namYeJi9q <nyj5850@naver.com>
2023-02-10 23:00:24 +0900

    🔨 Create Useditem

--- src/components/units/brand/write/index.tsx
+++ src/components/units/brand/write/index.tsx
@@ -1,10 +1,11 @@
 import { IProductWrite, IProductWriteProps } from "./write.types";
-import { useForm } from "antd/es/form/Form";
 import { useRouter } from "next/router";
 import * as S from "./write.styles";
 import dynamic from "next/dynamic";
 import { ICreateUseditemInput } from "@/src/commons/types/generated/types";
 import { useMutationCreateUseditem } from "@/src/components/commons/hooks/useMutation/product/useMutationCreateUseditem";
+import { useForm } from "react-hook-form";
+import { useMoveToPage } from "../../../commons/hooks/customs/useMoveToPage";
 
 const ReactQuill = dynamic(async () => await import("react-quill"), {
   ssr: false,
@@ -12,27 +13,27 @@ const ReactQuill = dynamic(async () => await import("react-quill"), {
 
 export default function ProductWrite(props: IProductWriteProps) {
   const router = useRouter();
-
+  const { onClickMoveToPage } = useMoveToPage();
   const { createUsedItemSubmit } = useMutationCreateUseditem();
-  // const { register, handleSubmit, setValue, trigger } =
-  //   useForm<IProductWrite>();
+  const { register, handleSubmit, setValue, trigger } =
+    useForm<ICreateUseditemInput>();
 
-  //   const onChangeContents = (value: string) => {
-  //     setValue("contents", value === "<p><br></p>" ? "" : value);
-  //     void trigger("contents");
-  //   };
+  const onChangeContents = (value: string) => {
+    setValue("contents", value === "<p><br></p>" ? "" : value);
+    void trigger("contents");
+  };
 
   return (
     <S.Wrapper>
       <h1>상품{props.isEdit ? "수정" : "등록"}</h1>
-      <S.WriteWrapper>
+      <S.WriteWrapper onSubmit={handleSubmit(createUsedItemSubmit)}>
         <div>
           <S.WriteList>
             <span>상품명</span>
             <input
               type="text"
               placeholder="상품명을 작성해주세요"
-              //   {...register("name")}
+              {...register("name")}
             />
           </S.WriteList>
           <S.WriteList>
@@ -40,14 +41,14 @@ export default function ProductWrite(props: IProductWriteProps) {
             <input
               type="text"
               placeholder="상품요약을 작성해주세요"
-              //   {...register("remarks")}
+              {...register("remarks")}
             />
           </S.WriteList>
           <S.WriteList>
             <span>상품 내용</span>
             <ReactQuill
               style={{ height: "300px" }}
-              //   onChange={onChangeContents}
+              onChange={onChangeContents}
             />
           </S.WriteList>
           <S.WriteList>
@@ -55,7 +56,7 @@ export default function ProductWrite(props: IProductWriteProps) {
             <input
               type="text"
               placeholder="판매 가격을 입력해주세요"
-              //   {...register("price")}
+              {...register("price")}
             />
           </S.WriteList>
           <S.WriteList>
@@ -63,16 +64,16 @@ export default function ProductWrite(props: IProductWriteProps) {
             <input
               type="text"
               placeholder="#태그 #태그 #태그"
-              //   {...register("tags")}
+              {...register("tags")}
             />
           </S.WriteList>
           <S.AddressList>
             <span>브랜드 위치</span>
             <div>
-              {/* <input type="text" {...register("useditemAddress.address")} /> */}
+              <input type="text" {...register("useditemAddress.address")} />
               <input
                 type="text"
-                // {...register("useditemAddress.addressDetail")}
+                {...register("useditemAddress.addressDetail")}
               />
             </div>
           </S.AddressList>
@@ -82,8 +83,10 @@ export default function ProductWrite(props: IProductWriteProps) {
           </S.FileUploadList>
         </div>
         <S.ButtonWrap>
-          <button>취소</button>
-          <button>수정</button>
+          <button type="button" onClick={onClickMoveToPage("/brand")}>
+            취소
+          </button>
+          <button type="submit">{props.isEdit ? "수정" : "등록"}</button>
         </S.ButtonWrap>
       </S.WriteWrapper>
     </S.Wrapper>
