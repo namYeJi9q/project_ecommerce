030b74c0394674fc3b0f66769048bbc0503f7185
namYeJi9q <nyj5850@naver.com>
2023-02-18 23:10:31 +0900

    Brand UI

--- src/components/units/brand/list/index.tsx
+++ src/components/units/brand/list/index.tsx
@@ -1,15 +1,24 @@
 import { useMoveToPage } from "@/src/components/commons/hooks/customs/useMoveToPage";
+import ProductItem from "@/src/components/commons/productItem";
+import * as S from "./list.styles";
 
 export default function BrandMain() {
   const { onClickMoveToPage } = useMoveToPage();
 
   return (
-    <>
+    <S.BrandWrapper>
+      <S.BestWrap>
+        <S.BestTitle>BEST</S.BestTitle>
+      </S.BestWrap>
       <div>
-        <h2>BEST</h2>
+        <S.SubmitButton onClick={onClickMoveToPage("/brand/new")}>
+          상품등록
+        </S.SubmitButton>
+        <div>검색</div>
       </div>
-      <button onClick={onClickMoveToPage("/brand/new")}>상품등록</button>
-      <div></div>
-    </>
+      <S.ProductItemWrap>
+        <ProductItem />
+      </S.ProductItemWrap>
+    </S.BrandWrapper>
   );
 }
---
+++ src/components/units/brand/list/list.styles.ts
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+
+export const BrandWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px 50px;
+`;
+
+export const BestWrap = styled.section`
+  width: 1822px;
+  height: 767px;
+  background-color: #fff;
+  padding: 40px;
+`;
+export const BestTitle = styled.h2`
+  font-size: 22px;
+  font-weight: 700;
+  margin: 0 auto;
+`;
+
+export const SubmitButton = styled.button`
+  background-color: #fff;
+  color: #000;
+  font-weight: 700;
+  font-size: 20px;
+  width: 195px;
+  height: 77px;
+  border: 1px solid #000;
+`;
+
+export const ProductItemWrap = styled.section`
+  width: 100%;
+`;
